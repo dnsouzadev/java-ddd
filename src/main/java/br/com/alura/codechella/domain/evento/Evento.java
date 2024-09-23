a237f8fb7ba479a185c3cbf90ba7aee62668f565
Daniel Souza <workdndsza@gmail.com>
2024-09-23 10:36:05 -0300

    separando as camadas por responsabilidade

--- src/main/java/br/com/alura/codechella/domain/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/domain/evento/Evento.java
@@ -1,7 +1,6 @@
 package br.com.alura.codechella.domain.evento;
 
-import br.com.alura.codechella.Categoria;
-import br.com.alura.codechella.Endereco;
+import br.com.alura.codechella.infra.evento.Endereco;
 import br.com.alura.codechella.domain.ingresso.TipoIngresso;
 
 import java.time.LocalDateTime;
